63050522682892fa2bdaaf9b0a6be007a13ebc80
ss75710541 <75710541@qq.com>
2017-04-18 17:32:55 +0800

    run_pybot add debug

--- run_pybot.sh
+++ run_pybot.sh
@@ -3,4 +3,15 @@ base_dir=$(cd `dirname $0` && pwd)
 cd $base_dir
 . ./config.cfg
 
-docker run --rm -i $AUTOTESTBORG_IMAGE $@ 
+if [ "$DEBUG" == "true" ];then
+	set +e
+	docker rm -f autotestborg
+	docker run -i --name autotestborg $AUTOTESTBORG_IMAGE $@
+	mkdir -p /data/offline-yumrepo/pybot
+	rm -f /data/offline-yumrepo/pybot/log.html
+	rm -f /data/offline-yumrepo/pybot/report.html
+	docker cp autotestborg:/BORSP_api/log.html /data/offline-yumrepo/pybot/
+	docker cp autotestborg:/BORSP_api/report.html /data/offline-yumrepo/pybot/
+else
+	docker run --rm -i $AUTOTESTBORG_IMAGE $@
+fi
