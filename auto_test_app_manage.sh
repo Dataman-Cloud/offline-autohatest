922310a25eaf1dcf79aff240c90d93b676ba07b8
ss75710541 <75710541@qq.com>
2017-05-15 14:30:12 +0800

    update BORSP_api 参数变量列表

--- auto_test_app_manage.sh
+++ auto_test_app_manage.sh
@@ -5,15 +5,9 @@ set -e
 . $SRY_DIR/config.cfg
 
 if [ "$OFFLINE_PACKAGE_MOD" == "marathon" ];then
-	./run_pybot.sh pybot \
-	$PYBOT_VARIABLES \
-	 --include ha valid_app_manage.txt
+	./run_pybot.sh pybot $PYBOT_VARIABLES --include ha valid_app_manage.txt
 fi
 
 if [ "$OFFLINE_PACKAGE_MOD" == "swan" ];then
-        PYBOT_VARIABLES="$PYBOT_VARIABLES --variable adminname:$SRY_HATEST_USER \
-       		--variable adminpasswd:$SRY_HATEST_PASSWD"
-	./run_pybot.sh pybot \
-	$PYBOT_VARIABLES \
-	 --include ha valid_app_v2.txt
+	./run_pybot.sh pybot $PYBOT_VARIABLES --include ha valid_app_v2.txt
 fi
